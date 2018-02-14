648508189884f7e41f54f3f95bcb3b98a6f91fb6
sidv1993 <sidv1993@gmail.com>
2018-02-13 23:37:41 -0600

    Added New Files

--- Arrays and Strings/Search2DMatrix.py
+++ Arrays and Strings/Search2DMatrix.py
@@ -1,8 +1,6 @@
 # Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 # Integers in each row are sorted from left to right.
-# The first integer of each row is greater than the last integer of the previous row.
-# For example,
-# Consider the following matrix:
+# The first integer of each row is greater than the last integer of the previous row. For example, consider the following matrix:
 # [
 #   [1,   3,  5,  7],
 #   [10, 11, 16, 20],
@@ -15,6 +13,14 @@
 # (2) Mid is middle of low, high. Convert it to [row, col] by [element no. / len(col), element no. % len(col)]
 # (3) Check if else and assign new values to low and high respectively
 # TC: O(log n)
+# SC: O(1)
+
+# *** MODIFIED QUESTION ***
+# Note: If row and column are sorted in increasing order i.e the entire matrix is not a sorted list, then
+# (1) Pick the top right element and check if equals target
+# (2) If target is greater then move down. If matrix out of bounds, return false.
+# (3) If target is lesser then move left. If matrix out of bounds, return false.
+# TC: O(n)
 # SC: O(1)
 
 class Solution(object):
