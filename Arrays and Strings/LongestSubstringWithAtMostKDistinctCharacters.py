033bc32cf2f6ff27579a4c07c91ac023d9bce8b2
sidv1993 <sidv1993@gmail.com>
2018-03-06 09:32:26 -0600

    Added Solutions

---
+++ Arrays and Strings/LongestSubstringWithAtMostKDistinctCharacters.py
@@ -0,0 +1,34 @@
+# Given a string, find the length of the longest substring T that contains at most k distinct characters.
+# For example, Given s = “eceba” and k = 2,
+# T is "ece" which its length is 3.
+
+# KEY STEP: Use Hashmap to store character frequency count of a sliding window.
+# - Keep extending right and updating hashmap and character count
+# - If unique characters exceeds k, extend left and keep updating hashmap
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        hm = {}
+        dc, maxl = 0, 0
+        i, j = 0, 0
+        while j < len(s):
+            if s[j] not in hm:
+                hm[s[j]] = 1
+                dc += 1
+                while dc > k:
+                    i += 1
+                    hm[s[i-1]] -= 1
+                    if hm[s[i-1]] == 0:
+                        hm.pop(s[i-1], None)
+                        dc -= 1
+            else:
+                hm[s[j]] += 1
+            maxl = max(maxl, sum(hm.values()))
+            j += 1
+        return maxl
+
---
+++ Miscellaneous/KEmptySlots.py
@@ -0,0 +1,45 @@
+# There is a garden with N slots. In each slot, there is a flower. The N flowers will bloom one by one in N days. In each day, there will be exactly one flower blooming and it will be in the status of blooming since then.
+# Given an array flowers consists of number from 1 to N. Each number in the array represents the place where the flower will open in that day.
+# For example, flowers[i] = x means that the unique flower that blooms at day i will be at position x, where i and x will be in the range from 1 to N.
+# Also given an integer k, you need to output in which day there exists two flowers in the status of blooming, and also the number of flowers between them is k and these flowers are not blooming.
+# If there isn't such day, output -1.
+
+# Example 1:
+# Input: 
+# flowers: [1,3,2]
+# k: 1
+# Output: 2
+# Explanation: In the second day, the first and the third flower have become blooming.
+
+# Example 2:
+# Input: 
+# flowers: [1,2,3]
+# k: 1
+# Output: -1
+# Note:
+# The given array will be in the range [1, 20000].
+
+class Solution:
+    def kEmptySlots(self, flowers, k):
+        """
+        :type flowers: List[int]
+        :type k: int
+        :rtype: int
+        """
+        ans = float('inf')
+        days = [0]*len(flowers)
+
+        for day, location in enumerate(flowers, 1):
+        	days[location-1] = day
+
+        left, right = 0, k + 1
+
+        while right < len(days):
+        	for i in range(left + 1, right):
+        		if days[i] < days[left] or days[i] < days[right]:
+        			left, right = i, i + k + 1
+        			break
+        	else:
+        		ans = min(ans, max(days[left], days[right]))
+        		left, right = right, right + k + 1
+        return ans if ans < float('inf') else -1
